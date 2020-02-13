53f17edfc116216ae18c7dad3f4d50d4ebceac30
Josh Colbert <jcolber@gmail.com>
2020-02-12 21:34:29 -0700

    Add files via upload

---
+++ templates/images.php
@@ -0,0 +1,101 @@
+
+        <div class="row">
+          <div class="portfolio-item">
+              <div class="col-sm-6 col-md-4 graphic-design">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/graphic-design/door-hanger.jpg" title="Overhead Door Marketing Material" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Door Flyer I Designed and Printed</h4>
+                              </div>
+                              <img src="images/graphic-design/door-hanger.jpg" class="img-responsive" alt="Door Hanger">
+                          </a>
+                        </div>
+                  </div>
+              </div>
+
+
+              <div class="col-sm-6 col-md-4 photography">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/photography/Josh-Colbert-40.jpg" title="Arches National Park, Utah" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Wilson Arch I think</h4>
+                              </div>
+                              <img src="images/photography/Josh-Colbert-40.jpg" class="img-responsive" alt="Arches National Park">
+                          </a>
+                      </div>
+                  </div>
+              </div>
+
+
+              <div class="col-sm-6 col-md-4 photography">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/photography/Monte-Cristo-Twin-Peaks-Lake-Blanche.jpg" title="Twin Peaks, Wasatch Mountains, Utah" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Twin Peaks overlooking Lake Blanche in the Wasatch Mountain Range, Salt Lake City, Utah</h4>
+                              </div>
+                              <img src="images/photography/Monte-Cristo-Twin-Peaks-Lake-Blanche.jpg" class="img-responsive" alt="Twin Peaks/Lake Blanche">
+                          </a> </div>
+                  </div>
+              </div>
+
+
+              <div class="col-sm-6 col-md-4 graphic-design">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/graphic-design/get-belayed-start-finish.jpg" title="Get Belayed Sticker" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Get Belayed Sticker Process</h4>
+                              </div>
+                              <img src="images/graphic-design/get-belayed.jpg" class="img-responsive" alt="Get Belayed">
+                          </a> </div>
+                  </div>
+              </div>
+
+
+              <div class="col-sm-6 col-md-4 graphic-design">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/graphic-design/start-to-finish.jpg" title="Snowboard Sticker" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Snowboard Sticker</h4>
+                              </div>
+                              <img src="images/graphic-design/Method-Silouhette.jpg" class="img-responsive" alt="Snowboard Sticker Design">
+                          </a> </div>
+                  </div>
+              </div>
+
+              <!-- billboard -->
+              <div class="col-sm-6 col-md-4 graphic-design">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/graphic-design/OHDbillboard.jpg" title="Snowboard Sticker" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Snowboard Sticker</h4>
+                              </div>
+                              <img src="images/graphic-design/OHDbillboard.jpg" class="img-responsive" alt="Snowboard Sticker Design">
+                          </a> </div>
+                  </div>
+              </div>
+              <!-- garage works -->
+              <div class="col-sm-6 col-md-4 graphic-design">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/graphic-design/van.jpg" title="Snowboard Sticker" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Snowboard Sticker</h4>
+                              </div>
+                              <img src="images/graphic-design/van.jpg" class="img-responsive" alt="Snowboard Sticker Design">
+                          </a> </div>
+                  </div>
+              </div>
+              <!-- ski lift bottle opener -->
+              <div class="col-sm-6 col-md-4 graphic-design">
+                  <div class="portfolio-item">
+                      <div class="hover-bg"> <a href="images/graphic-design/bottle-openers.png" title="Magnetic Bottle Openers" data-lightbox-gallery="gallery1">
+                              <div class="hover-text">
+                                  <h4>Bottle Opener Ideas</h4>
+                              </div>
+                              <img src="images/graphic-design/bottle-openers.png" class="img-responsive" alt="Bottle Openers">
+                          </a> </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
